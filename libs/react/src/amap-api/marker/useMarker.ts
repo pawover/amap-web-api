620d463b294f41f279b60d1ef7879b463bd8f496
Handpear <handpear@outlook.com>
2023-09-21 15:48:37 +0800

    fix(amap-react): 修复 Marker 组件子内容渲染错误
    修复 children 渲染内容可能出现空字符串而不是正确内容

--- libs/react/src/amap-api/marker/useMarker.ts
+++ libs/react/src/amap-api/marker/useMarker.ts
@@ -8,13 +8,14 @@ interface UseMarkerProps extends MarkerProps {}
 
 export const useMarker = (props: UseMarkerProps) => {
   const { visible = true, content, children, ...rest } = props;
+  const renderContent = content || children;
   const { map } = useMapContext();
   const [marker, setMarker] = useState<AMap.Marker>();
   const [wrapper, setWrapper] = useState<React.ReactPortal>();
   const isStrictModeRenderedRefRef = useRef(false);
 
   useVisible(marker, visible);
-  useProperty<AMap.Marker, UseMarkerProps>(marker, { ...props, content: content || children ? ' ' : undefined });
+  useProperty<AMap.Marker, UseMarkerProps>(marker, rest);
   useEventProperty<AMap.Marker, UseMarkerProps, AMap.Marker.Events>(marker, props, [
     'onHide',
     'onShow',
@@ -40,18 +41,18 @@ export const useMarker = (props: UseMarkerProps) => {
   useEffect(() => {
     if (AMap && map && !marker && !isStrictModeRenderedRefRef.current) {
       const options: AMap.Marker.Options = rest;
-      if (content || children) {
-        options.content = ' ';
-      }
       const instance = new AMap.Marker(options);
       map.add(instance);
-      if (content || children) {
+
+      if (renderContent) {
         const dom = instance.getContentDom();
         if (dom) {
-          const wrapper = createPortal(content || children, dom);
+          dom.innerHTML = '';
+          const wrapper = createPortal(renderContent, dom);
           setWrapper(wrapper);
         }
       }
+
       setMarker(instance);
       isStrictModeRenderedRefRef.current = true;
     }
@@ -64,6 +65,16 @@ export const useMarker = (props: UseMarkerProps) => {
       }
     };
   }, [map, marker]);
+
+  useEffect(() => {
+    if (marker && renderContent) {
+      const dom = marker.getContentDom();
+      if (dom) {
+        const wrapper = createPortal(renderContent, dom);
+        setWrapper(wrapper);
+      }
+    }
+  }, [marker, renderContent]);
 
   return { marker, wrapper };
 };
