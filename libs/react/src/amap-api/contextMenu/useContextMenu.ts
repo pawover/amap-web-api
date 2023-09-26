1b951294170610550ca1f15ebb6f52b3cefffb3f
Handpear <handpear@outlook.com>
2023-09-26 18:49:39 +0800

    perf(amap-react): 优化 event 判断

--- libs/react/src/amap-api/contextMenu/useContextMenu.ts
+++ libs/react/src/amap-api/contextMenu/useContextMenu.ts
@@ -19,8 +19,9 @@ export const useContextMenu = (props: UseContextMenu) => {
     if (AMap && map && !contextMenu) {
       const instance = new AMap.ContextMenu(rest);
       setContextMenu(instance);
-      const rightClick = (event?: AMap.MapsEvent<AMap.MapEventList, unknown>) =>
-        instance.open(map, position || [event.lnglat!?.lng, event.lnglat!?.lat]);
+      const rightClick = (event?: AMap.MapsEvent<AMap.MapEventList, unknown>) => {
+        if (event?.lnglat) instance.open(map, position || event.lnglat.toArray());
+      };
       map.on('rightclick', rightClick);
     }
     return () => {
